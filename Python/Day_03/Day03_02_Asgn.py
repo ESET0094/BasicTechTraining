9ad86831400d779c23f1925502ce54df2cd56ee6
ESET0094 <sarath.chandra@esyasoft.com>
2025-09-03 15:34:06 +0530

    Uploading Day 3 Sessions

---
+++ Python/Day_03/Day03_02_Asgn.py
@@ -0,0 +1,7 @@
+#Program to Check if number is even or odd by iterating in a list
+x=[98,23,56,7,86,43,12,34,76]
+for i in x:
+    if i%2 ==0:
+        print(i," is even")
+    else:
+        print(i," is odd")
